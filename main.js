7e601e755a33ec86f8733d816c05331a2f59e0a9
starbrat <43695821+starbrat@users.noreply.github.com>
2022-09-14 01:22:50 +0300

    Added the file browser

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain, dialog } = require("electron");
+const fs = require('fs-extra');
+const path = require('path');
 
 let mainWindow;
 
@@ -20,6 +22,7 @@ function createWindow() {
         height: 600,
         webPreferences: {
             nodeIntegration: true,
+            contextIsolation: false
         },
         icon: "public/favicon.png",
     });
@@ -37,4 +40,45 @@ app.on("window-all-closed", function () {
 
 app.on("activate", function () {
     if (mainWindow === null) createWindow();
+});
+
+function readSelected(directory, event) {
+    let processedFiles = [];
+
+    fs.readdir(directory, { withFileTypes: true }, (err, files) => {
+        for (var i = 0; i < files.length; i++) {
+            if (files[i].isDirectory()) {
+                processedFiles.push({
+                    name: files[i].name, isDirectory: 1, addr: path.join(directory, files[i].name)
+                });
+            }
+            else {
+                processedFiles.push({
+                    name: files[i].name, isDirectory: 0, addr: path.join(directory, files[i].name)
+                });
+            }
+        }
+
+        event.sender.send('directory', {
+            directory,
+            files: processedFiles
+        });
+    });
+}
+
+ipcMain.on('select', (event, arg) => {
+    if (arg) return readSelected(arg, event);
+
+    dialog.showOpenDialog(mainWindow, {
+        title: "Select directory",
+        properties: ['openDirectory']
+    }).then(result => {
+        if (!result.cancelled && result.filePaths.length > 0) {
+            let directory = result.filePaths[0];
+
+            readSelected(directory, event);
+        }
+    }).catch(err => {
+        console.log("Error opening file: ", err);
+    });
 });
--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,146 @@
 <script>
+	const { ipcRenderer } = require('electron');
+	const path = require('path');
+
+	let directory;
+	let files;
+	let before;
+
+	ipcRenderer.on('directory', (event, arg) => {
+		directory = arg.directory;
+		files = arg.files;
+		before = path.normalize(path.join(directory, '..'));
+
+		console.log(before);
+	});
 </script>
 
 <main>
-	MVP here!
+	{#if !directory}
+		<div class="directory-selector">
+			<button on:click={() => ipcRenderer.send('select')}>Select a directory</button>
+		</div>
+	{:else}
+		<div class="file-browser">
+			<div class="directory-url">{directory}</div>
+			<ul class="directory-files">
+				{#if before}
+					<li>
+						<a
+							class="file"
+							href="{before}"
+							on:click|preventDefault={
+								() => ipcRenderer.send('select', before)
+							}
+						>
+							<span class="file-icon">←</span>
+							<span class="file-name">..</span>
+						</a>
+					</li>
+				{/if}
+				{#each files as file}
+					<li>
+						{#if file.isDirectory}
+							<a
+								class="file"
+								href="{file.addr}"
+								on:click|preventDefault={
+									() => ipcRenderer.send('select', file.addr)
+								}
+							>
+								<span class="file-icon">🗀</span>
+								<span class="file-name">{file.name}</span>
+							</a>
+						{:else}
+							<span
+								class="file"
+							>
+								<span class="file-icon">🗎</span>
+								<span class="file-name">{file.name}</span>
+							</span>
+						{/if}
+					</li>
+				{/each}
+			</ul>
+		</div>
+	{/if}
 </main>
 
 <style lang="scss">
+	main {
+		position: fixed;
+		left: 0;
+		top: 0;
+		right: 0;
+		bottom: 0;
+	}
+
+	.directory-selector {
+		position: fixed;
+		left: 0;
+		top: 0;
+		right: 0;
+		bottom: 0;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.file-browser {
+		padding: 5px;
+		display: flex;
+		flex-direction: column;
+		height: 100%;
+	}
+
+	.directory-url {
+		flex-shrink: 0;
+		border: 1px solid black;
+		padding: 3px 8px;
+		margin-bottom: 5px;
+	}
+
+	.directory-files {
+		flex-shrink: 0;
+		margin: 0;
+		flex-grow: 1;
+		padding: 5px;
+		border: 1px solid black;
+		display: flex;
+		flex-wrap: wrap;
+		list-style: none;
+		align-content: flex-start;
+
+		li {
+			display: inline-flex;
+			align-self: flex-start; 
+		}
+	}
+
+	.file {
+		padding: 3px 3px 1px;
+		display: inline-flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: flex-start;
+		font-family: Helvetica, sans-serif;
+		text-decoration: none;
+		width: 55px;
+		height: 55px;
+		align-self: flex-start; 
+
+		&-icon {
+			font-size: 30px;
+		}
+
+		&-name {
+			font-size: 10px;
+			word-break: break-all;
+			max-width: 55px;
+			text-align: center;
+			max-height: 20px;
+			text-overflow: ellipsis;
+			overflow: hidden;
+		}
+	}
 </style>
